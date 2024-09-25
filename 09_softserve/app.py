fe4d317207b1847d34b3c667dfaf930f1160a61b
Nia Lam <nlam50@stuy.edu>
2024-09-25 08:03:25 -0400

    updated k09

--- 09_softserve/app.py
+++ 09_softserve/app.py
@@ -7,26 +7,19 @@
 
 '''
 DISCO:
-Flask creates the website that runs the hello_world so we can see the "No hable queso!". It needs the virtual machine to run.
+- return to output on web
+- @app.route("/") front
 
 QCC:
-0. creating a flask object with a name? like java?
-1. Root Directory? or to seperate directories in a path
-2. probably the shell? I don't see anything that could be __name__ in the website or terminal.
-3. a name, it might be the website name or something that shows up in the terminal when ran.
-4. it appears on the website
-5. java!?!?! function from an object?
+0. how to return multiple print statements?
  ...
 
 INVESTIGATIVE APPROACH:
-Demoer showed that the scary looking terminal output is a link that works (:
 '''
-import csv
-import random
-
 from flask import Flask
 
-app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs?
+import csv
+import random
 
 with open("occupations.csv", newline="") as csvfile:
     #creates a dictionary for every row that can be parsed through
@@ -36,12 +29,6 @@ with open("occupations.csv", newline="") as csvfile:
     for row in reader:
         jobs.append(row['Job Class']), percents.append(float(row['Percentage']))
         
-@app.route("/")                          # Q1: What points of reference do you have for meaning of '/'?
-
-# def ReturnRandom():
-# # random.choices returns a list, [:-1] to ignore last row, k is returned list size
-#     return (random.choices(jobs[:-1], weights=percents[:-1], k=1)[0])
-
 def RandomManual():
     # find a valid percentage, subtract percents until random <=0 and we can return the corresponding jobs
     rand = random.random() * percents[-1]
@@ -49,21 +36,25 @@ def RandomManual():
         rand -= percents[i]
         if rand <= 0:
             return jobs[i]
-# def hello_world():
-#     print(__name__)                      # Q2: Where will this print to? Q3: What will it print?
-#     return "No hablo queso!"             # Q4: Will this appear anywhere? How u know?
 
-#print(ReturnRandom())
-
-def Write():
-    print("Team 36: Stanley Hoo, Nia Lam, Jady Lei")
-    print(RandomManual())
-    print("\n")
-    print(jobs)
-    
-print(Write())
-
-app.run()                                # Q5: Where have you seen similar constructs in other languages?
+def htmlOut():
+    output = "<h1>63</h1>\n"
+    output += "<p>Period 4</p>\n"
+    output += "<ul>"
+    for job in jobs[:-1]:
+        output += "\t <li>"
+        output += job
+        output += "</li> \n"
+    output += "</ul> \n"
+    return output
 
 
+app = Flask(__name__)        
 
+@app.route("/")                         
+def hello_world():
+    print(__name__)                  
+    return htmlOut()           
+
+app.debug = True
+app.run()       
