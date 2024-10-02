e763b9a872f2716a350f4e481cc727f43f7e94b5
Nia Lam <nlam50@stuy.edu>
2024-10-01 23:37:37 -0400

    updated links code and csv

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -12,15 +12,34 @@ import csv
 from flask import Flask, render_template
 app = Flask(__name__)
 
-d = {}
+
+d_list = []
+d_percentage = []
+def getPercentage(d_list):
+    l = []
+    for dic in d_list:
+        l.append(dic.get('percentage'))
+    # print(l)
+    return l
+
 def randocc():
     with open("data/occupations.csv", "r") as file:
-        arr = list(csv.reader(file))[1:-2] #omit first and last lines
+        arr = list(csv.reader(file))[1:-1] #omit first and last lines
     for i in arr:
-        d.update({i[0]:float(i[1])})
-    d.update({"Ducky":0.2}) #make the total 100%
-    random_occ = random.choices(list(d.keys()), list(d.values()))[0]
-    return random_occ
+        d = {}
+        d.update({"occupation":i[0]})
+        d.update({"percentage":float(i[1])})
+        d.update({"link":i[2]})
+        #d.update({i[0]:float(i[1])})
+        #print(d)
+        #print("/n")
+        d_list.append(d)
+    #d.update({"Ducky":0.2}) #make the total 100%
+    #print(d)
+    d_percentage = getPercentage(d_list)
+
+    random_occ = random.choices(d_list, d_percentage)[0]
+    return random_occ.get('occupation')
 
 @app.route("/")
 
@@ -34,13 +53,13 @@ def randocc():
 
 @app.route("/wdywtbwygp")
 def test_tmplt():
+    
     return render_template('tablified.html',
                            foo = "random occupations generator",
                            rand = randocc(),
-                           dictionary = d)
+                           dictionary = d_list)
 
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
