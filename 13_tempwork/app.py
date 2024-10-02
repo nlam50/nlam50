a164e96e5a4c4387bd4d8ba7ad58515a8b6bd8eb
Nia Lam <nlam50@stuy.edu>
2024-10-02 02:32:41 -0400

    updated k13 python links and reading notes

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -1,63 +1,41 @@
-# peaches & mangoes
+# peaches & mangoes: Anastasia Lee, Nia Lam, Naomi Lai
 # SoftDev
-# Sep 2024
+# Oct 2024
 
-"""
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-"""
 import random
 import csv
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-d_list = []
-d_percentage = []
-def getPercentage(d_list):
-    l = []
-    for dic in d_list:
-        l.append(dic.get('percentage'))
-    # print(l)
-    return l
-
+d = {}
 def randocc():
+    percentages = []
     with open("data/occupations.csv", "r") as file:
         arr = list(csv.reader(file))[1:-1] #omit first and last lines
+        #print(arr)
     for i in arr:
-        d = {}
-        d.update({"occupation":i[0]})
-        d.update({"percentage":float(i[1])})
-        d.update({"link":i[2]})
-        #d.update({i[0]:float(i[1])})
-        #print(d)
-        #print("/n")
-        d_list.append(d)
-    #d.update({"Ducky":0.2}) #make the total 100%
-    #print(d)
-    d_percentage = getPercentage(d_list)
+        d.update({i[0]:[float(i[1]), i[2]]})
+        percentages.append(float(i[1]))
+    d.update({"Ducky":[0.2,"https://www.peta.org/features/facts-about-ducks/"]}) #make the total 100%
+    percentages.append(0.2)
+    # print(len(percentages))
+    # print(len(list(d.keys())))
+    random_occ = random.choices(list(d.keys()), list(percentages))[0]
+    return random_occ
 
-    random_occ = random.choices(d_list, d_percentage)[0]
-    return random_occ.get('occupation')
+# print(randocc())
 
 @app.route("/")
+def hello():
+    return  "<a href=\"http://127.0.0.1:5000/wdywtbwygp\">random occupations generator</a>"
 
-# def hello_world():
-#     txt = "Team 54<br>Anastasia, Mark, Brian<br>"
-#     txt += "<h2> A random occupation is chosen: " + randocc() + "</h2><br><br>"
-#     txt += "<h3>Here is the list of occupations and their percentages:</h3><br>"
-#     for key, values in d.items():
-#         txt += key + '<div align="center">' + str(values) + "</div> <br>"
-#     return txt
 
 @app.route("/wdywtbwygp")
 def test_tmplt():
-    
     return render_template('tablified.html',
                            foo = "random occupations generator",
                            rand = randocc(),
-                           dictionary = d_list)
+                           dictionary = d)
 
 
 if __name__ == "__main__":
