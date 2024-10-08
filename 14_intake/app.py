4c3574385c8c7743e7821a795415b1cc69ae5822
Nia Lam <nlam50@stuy.edu>
2024-10-07 23:22:18 -0400

    updated k14

--- 14_intake/app.py
+++ 14_intake/app.py
@@ -1,6 +1,6 @@
-# Clyde 'Thluffy' Sinclair
+# peaches & mangoes: Anastasia Lee, Nia Lam, Naomi Lai
 # SoftDev
-# October 2024
+# Oct 7 2024
 
 # import conventions:
 # list most general first (standard python library)
@@ -39,7 +39,7 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") # , methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -49,13 +49,13 @@ def disp_loginpage():
     print("***DIAG: request.args ***")
     print(request.args)
     print("***DIAG: request.args['username']  ***")
-#     print(request.args['username'])
+    # print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
     return render_template( 'login.html' )
 
 
-@app.route("/auth") # , methods=['GET', 'POST'])
+@app.route("/auth" , methods=['GET', 'POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -68,7 +68,7 @@ def authenticate():
     print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return "Waaaa hooo HAAAH"  #response to a form submission
+    return request.args['username']  #response to a form submission
 
 
     
