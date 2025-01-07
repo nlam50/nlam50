c5b7c7f7f5aee9bfdcf13c16fb5e6846188ae473
Nia Lam <nlam50@stuy.edu>
2025-01-07 11:03:38 -0500

    updated w27

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -1,10 +1,30 @@
+//TNPG: Nia Lam and Naomi Lai
+//SoftDev pd0
+//K27 - Basic functions in JavaScript
+//2025-01-06m
+
+//JavaScript implementations of Day0 recursive Scheme functions
+
+//factorial:
+
 var fact = function(n) {
 	if (n==1){
-		return n;	
+		return n;
 	}
 	else return (n * fact(n-1));
 }
 
+
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+fact(1)
+fact(2)
+fact(3)
+fact(4)
+//-----------------------------------------------------------------
+
+
+//fib:
 
 var fib = function(n) {
 	if (n == 0) {
@@ -16,3 +36,10 @@ var fib = function(n) {
 	}
 	else return ((fib(n -1)) + (fib(n -2)));
 }
+//TEST CALLS
+// (writing here can facilitate EZer copy/pasting into dev console now and later...)
+fib(0)
+fib(1)
+fib(2)
+fib(3)
+//=================================================================
