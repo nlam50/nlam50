ec9b4633dac4a4908d3db090292947048df00887
Nia Lam <nlam50@stuy.edu>
2025-03-14 11:47:57 -0400

    updated

--- 35_mxrobbotto/app/forms.py
+++ 35_mxrobbotto/app/forms.py
@@ -1,4 +1,26 @@
-from flask_wtf import FlaskForm
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+# from wtforms.validators import InputRequired, Length
+#
+# class RegisterForm(FlaskForm):
+#     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+#     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+#     submit = SubmitField('Register')
+#
+# class LoginForm(FlaskForm):
+#     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+#     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+#     submit = SubmitField('Login')
+#
+# class BlogForm(FlaskForm):
+#     title = StringField('Title', validators=[InputRequired(), Length(min=1, max=100)])
+#     content = TextAreaField('Content', validators=[InputRequired()])
+#     submit = SubmitField('Submit')
+
+Here's the missing `forms.py` file:
+
+```python name=app/forms.py
+from flask import Flask, render_template, request, session, redirect, url_for
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import InputRequired, Length
 
@@ -16,3 +38,6 @@ class BlogForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired(), Length(min=1, max=100)])
     content = TextAreaField('Content', validators=[InputRequired()])
     submit = SubmitField('Submit')
+```
+
+This file should be placed in the `app/` directory along with the other Python files.
